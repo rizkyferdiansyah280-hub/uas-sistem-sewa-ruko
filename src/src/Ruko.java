1ef0d9e3d17bf0adb8ad1c9cadc994b75f5cc09c
rizkyferdiansyah280-hub <rizkyferdiansyah280@gmail.com>
2026-02-01 21:18:58 +0700

    add Ruko class

---
+++ src/src/Ruko.java
@@ -0,0 +1,11 @@
+public class Ruko {
+    String kodeRuko;
+    String alamat;
+    double hargaSewa;
+
+    public Ruko(String kodeRuko, String alamat, double hargaSewa) {
+        this.kodeRuko = kodeRuko;
+        this.alamat = alamat;
+        this.hargaSewa = hargaSewa;
+    }
+}
